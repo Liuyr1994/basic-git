48bab98ae7af18397d9bfdc4bb62d9d21ba0dbc9
Henadzi Bessonay <s.client@mail.ru>
2025-01-19 10:46:22 +0300

    Удалена возможность операции деления.

--- src/main/java/ru/otus/java/basic/git/MainApplication.java
+++ src/main/java/ru/otus/java/basic/git/MainApplication.java
@@ -10,7 +10,7 @@ public class MainApplication {
             int a = scanner.nextInt();
             System.out.println("Введите второе число: ");
             int b = scanner.nextInt();
-            System.out.println("Введите номер операции: 1 +, 2 -, 3 *, 4 / ");
+            System.out.println("Введите номер операции: 1 +, 2 -, 3 *");
             int operation = scanner.nextInt();
             if (operation == 1) {
                 int result = a + b;
@@ -21,10 +21,6 @@ public class MainApplication {
             } else if (operation == 3) {
                 int result = a * b;
                 System.out.println(a + " * " + b + " = " + result);
-            } else {
-                int result = a / b;
-                System.out.println(a + " / " + b + " = " + result);
-            }
             System.out.println("Хотите ли вы выполнить ещё одну операцию?");
             String answer = scanner.next();
             if (!answer.equals("y")) {
