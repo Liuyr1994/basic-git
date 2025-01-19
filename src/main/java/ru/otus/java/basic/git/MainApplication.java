85346a77e17137cc9badaff40cc13e3223195e5c
Henadzi Bessonay <s.client@mail.ru>
2025-01-19 10:44:17 +0300

    Возможность выполнять несколько операций за один запуск проект.

--- src/main/java/ru/otus/java/basic/git/MainApplication.java
+++ src/main/java/ru/otus/java/basic/git/MainApplication.java
@@ -5,25 +5,33 @@ import java.util.Scanner;
 public class MainApplication {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        System.out.println("Введите первое число: ");
-        int a = scanner.nextInt();
-        System.out.println("Введите второе число: ");
-        int b = scanner.nextInt();
-        System.out.println("Введите номер операции: 1 +, 2 -, 3 *, 4 / ");
-        int operation = scanner.nextInt();
-        if (operation == 1) {
-            int result = a + b;
-            System.out.println(a + " + " + b + " = " + result);
-        } else if (operation == 2) {
-            int result = a - b;
-            System.out.println(a + " - " + b + " = " + result);
-        } else if (operation == 3) {
-            int result = a * b;
-            System.out.println(a + " * " + b + " = " + result);
-        } else {
-            int result = a / b;
-            System.out.println(a + " / " + b + " = " + result);
+        while (true) {
+            System.out.println("Введите первое число: ");
+            int a = scanner.nextInt();
+            System.out.println("Введите второе число: ");
+            int b = scanner.nextInt();
+            System.out.println("Введите номер операции: 1 +, 2 -, 3 *, 4 / ");
+            int operation = scanner.nextInt();
+            if (operation == 1) {
+                int result = a + b;
+                System.out.println(a + " + " + b + " = " + result);
+            } else if (operation == 2) {
+                int result = a - b;
+                System.out.println(a + " - " + b + " = " + result);
+            } else if (operation == 3) {
+                int result = a * b;
+                System.out.println(a + " * " + b + " = " + result);
+            } else {
+                int result = a / b;
+                System.out.println(a + " / " + b + " = " + result);
+            }
+            System.out.println("Хотите ли вы выполнить ещё одну операцию?");
+            String answer = scanner.next();
+            if (!answer.equals("y")) {
+                break;
+            }
         }
+        System.out.println("Работа калькулятора завершена!");
         scanner.close();
     }
 }
